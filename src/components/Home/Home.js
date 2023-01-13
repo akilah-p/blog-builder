0bc96382a1cba12f27a6d92992dc90cf4e66dc8c
Akilah <atpjun@gmail.com>
2023-01-13 12:58:49 -0500

    finally passing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview title={title} subTitle={subTitle} font={font} align={align} text={text}/>
+      <Preview title={title} subTitle={subTitle} font={font} align={align} text={text} />
       <Editor setTitle={setTitle}
         setSubTitle={setSubTitle}
         setFont={setFont}
